994e0822ad1ddab5deb631bdbcfbb5b534a5c4e2
ricardomayerb <ricardomayerb@gmail.com>
2019-10-16 15:51:21 -0300

    new puntos

--- sol1_ie_solucion.R
+++ sol1_ie_solucion.R
@@ -2,16 +2,16 @@ library(readxl)
 
 
 # 1. Use la función read_excel para cargar los datos que se encuentran en el archivo excel
-#  datos_ie.xlsx
+#  datos_ie.xlsx (0 pts)
 
 misdatos <- read_excel("datos_ie.xlsx")
 
 
 # 2. Para la variable "velocidad_viento", que es una de las columnas de los datos 
-# que acaba de cargar, encuentre lo siguiente:
-# 2.1 el cuantil 0.025
-# 2.2 la mediana
-# 2.3 el cuantil 0.975
+# que acaba de cargar, encuentre lo siguiente usando la función quantile:
+# 2.1 el cuantil 0.025 (2 pto)
+# 2.2 la mediana (2 pto)
+# 2.3 el cuantil 0.975 (2 pto)
 # Nota la función quantile es la que calcula cuantiles de un vector. Por ejemplo si el vector se llama x y
 # queremos calcular el cuantil 37 escribiremos en R quantile(x, 0.37)
 
@@ -24,10 +24,10 @@ c(vq025, vq500, vq975)
 
 
 # 3. Para la variable "temperatura" compute lo siguiente
-# 3.1 la media 
-# 3.2 la desviación estandar
-# 3.3 la cantidad de observaciones (n)
-# 3.4 la desviación estandar, dividida por la raíz de n
+# 3.1 la media (2 pto)
+# 3.2 la desviación estandar    (2 pto)
+# 3.3 la cantidad de observaciones (n)   (2 pto)
+# 3.4 la desviación estandar, dividida por la raíz de n  (2 pto)
 
   
 
@@ -42,8 +42,7 @@ c(tmu, tsd, n, tsdn)
 
 
 # 4. Grafique los histogramas de estas cuatro variables: temperatura, humedad,
-# punto_rocio y velocidad_viento. Puede usar cualquier función y paquete de R 
-# que grafique histogramas
+# punto_rocio y velocidad_viento. Puede usar cualquier función y paquete de R (4 pts)
 
 hist(misdatos$punto_rocio, breaks = 21)
 hist(misdatos$temperatura, breaks = 21)
@@ -53,7 +52,7 @@ hist(misdatos$velocidad_viento, breaks = 21)
 
 # 5. Use el comando pnorm, para encontrar la probabilidad de que una variable aleatoria normal
 # estándard esté entre -1 y 1.2, es decir, encontrar P(-1 <= z <= 1.2)
-# Nota, el comando pnorm asume por defecto que nuestra normal es estándar.
+# Nota, el comando pnorm asume por defecto que nuestra normal es estándar. (2 pto)
 
 pnorm(-1)
 pnorm(1.2)
